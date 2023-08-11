f4bec90e449054e2c38201e095ee2bc74e2d31d8
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-11 15:17:04 +0300

    Automapper için OrderDetailProfile, OrderProfile ve AddressProfile maplemeleri oluşturuldu ve yazıldı. DTO'dan Entity'e, Entity'den DTO'ya dönüşüm.

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Mappings/OrderDetailProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CasgemMicroservice.Services.Order.Core.Application.DTOs.OrderDetailsDTOs;
+using CasgemMicroservice.Services.Order.Core.Application.DTOs.OrderDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Mappings
+{
+    public class OrderDetailProfile : Profile
+    {
+        public OrderDetailProfile()
+        {
+            CreateMap<Domain.Entities.OrderDetail, ResultOrderDetailDTO>().ReverseMap();
+            CreateMap<Domain.Entities.OrderDetail, CreateOrderDetailDTO>().ReverseMap();
+            CreateMap<Domain.Entities.OrderDetail, UpdateOrderDetailDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Mappings/OrderProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CasgemMicroservice.Services.Order.Core.Application.DTOs.AddressDTOs;
+using CasgemMicroservice.Services.Order.Core.Application.DTOs.OrderDTOs;
+using CasgemMicroservice.Services.Order.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Mappings
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Domain.Entities.Order, ResultOrderDTO>().ReverseMap();
+            CreateMap<Domain.Entities.Order, CreateOrderDTO>().ReverseMap();
+            CreateMap<Domain.Entities.Order, UpdateOrderDTO>().ReverseMap();
+        }
+    }
+}
