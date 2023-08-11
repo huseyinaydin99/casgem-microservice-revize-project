09bfce35fbd1d9735d75b322c9f91f6b3a05e50c
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-11 14:56:53 +0300

    IRepository interface'i Application katmanına eklendi ve metot imzaları yazıldı.

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Interfaces
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<T> GetById(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> CreateAsync(T t);
+        Task<T> UpdateAsync(T t);
+        Task<T> DeleteAsync(T t);
+    }
+}
