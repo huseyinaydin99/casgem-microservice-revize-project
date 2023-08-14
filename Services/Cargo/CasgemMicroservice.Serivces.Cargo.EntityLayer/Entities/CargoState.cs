7408cae3ff3970f6bfab7c3c888f5302a1bdf64a
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-14 16:51:32 +0300

    Cargo Microservice katmanlar oluşturuldu CargoState ve CargoStateDetail Entity'leri yazıldı. İki Entity'de IEntity interface'sinden implemente etmekteler.

---
+++ Services/Cargo/CasgemMicroservice.Serivces.Cargo.EntityLayer/Entities/CargoState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Serivces.Cargo.EntityLayer.Entities
+{
+    public class CargoState : IEntity
+    {
+        public int CargoStateId { get; set; }
+        public string Description { get; set; }
+    }
+}
