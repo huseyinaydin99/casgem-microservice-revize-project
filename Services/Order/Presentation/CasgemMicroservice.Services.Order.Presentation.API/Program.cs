056d96cb1819db8c3cb80c3a02fb4a233adaf8a8
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-14 14:05:43 +0300

    Presentation kısmının JWT configurastion'u yazıldı.

--- Services/Order/Presentation/CasgemMicroservice.Services.Order.Presentation.API/Program.cs
+++ Services/Order/Presentation/CasgemMicroservice.Services.Order.Presentation.API/Program.cs
@@ -4,12 +4,28 @@ using CasgemMicroservice.Services.Order.Infrastructure.Persistance.Context;
 using CasgemMicroservice.Services.Order.Infrastructure.Persistance.Repositories;
 using System.Reflection;
 using CasgemMicroservice.Services.Order.Core.Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "resource_order";
+    opt.RequireHttpsMetadata = false;
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
